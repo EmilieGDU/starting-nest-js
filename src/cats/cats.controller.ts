a570fa20c7972d95ae75272a78ee71a05db9d787
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-11-23 11:08:48 +0100

    Routing : Express -> Request object

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
     @Get()
-    findAll(): string {
+    /* 
+    Handlers often need access to the client request details. Nest provides access to the request object of the underlying platform (Express by default). We can access the request object by instructing Nest to inject it by adding the @Req() decorator to the handler's signature.
+    In order to take advantage of Express typings (as in the request: Request parameter below), we need to install @types/express package.
+    */ 
+    findAll(@Req() request: Request): string {
         return 'This action returns all cats';
     }
 }
--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('customers') // 'customers' is a prefix declared for every route -> it's a path prefix
 export class CustomersController {
+    // @Get HTTP request method decorator BEFORE the findAll() method tells Nest to create a handler for a specific endpoint for HTTP requests.
+    // The endpoint corresponds to the HTTP request method (GET in this case) and the route path.
     @Get('profile') // 'profile' is a path information added in the decorator -> in this case, the path prefix of 'customers' combined with the decorator of @Get('profile') would produce a route mapping for requests like GET /customers/profile. And in this example, when a GET request is made to this endpoint, Nest routes the request to the user-defined findAll() method.
     findAll(): string {
         return 'This action returns the profile of the customer';
