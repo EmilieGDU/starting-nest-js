201fabfe966f4dec93bcb76266d2cfdffe639b71
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-11-23 10:41:22 +0100

    Découverte des contrôleurs et du routage

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersController } from './customers/customers.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CustomersController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
