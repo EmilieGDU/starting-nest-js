b2318c80cb545de5be00ed1abd4b18daa1722e17
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-11-23 10:52:04 +0100

    Routing : Gestionnaire de route -> prefix + path information

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('customers')
-export class CustomersController {}
+@Controller('customers') // 'customers' is a prefix declared for every route -> it's a path prefix
+export class CustomersController {
+    @Get('profile') // 'profile' is a path information added in the decorator -> in this case, the path prefix of 'customers' combined with the decorator of @Get('profile') would produce a route mapping for requests like GET /customers/profile. And in this example, when a GET request is made to this endpoint, Nest routes the request to the user-defined findAll() method.
+    findAll(): string {
+        return 'This action returns the profile of the customer';
+    }
+}
