3b3f39bf8cb52b6d5b99f7b26168080418cf3417
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-11-23 13:27:31 +0100

    Routing : Paramètres de route

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
@@ -9,13 +9,27 @@ export class CatsController {
         return 'This action adds a new cat'
     }
     
-    // Endpoint to fetch the cats resource (GET route)
+    // Endpoints to fetch the cats resources (GET routes)
+    // Static path (without params)
     @Get()
     /*  Handlers often need access to the client request details. Nest provides access to the request object of the underlying platform (Express by default). We can access the request object by instructing Nest to inject it by adding the @Req() decorator to the handler's signature.
     In order to take advantage of Express typings (as in the request: Request parameter below), we need to install @types/express package. */ 
     findAll(@Req() request: Request): string {
         return 'This action returns all cats';
     }
+
+    // Dynamic path (route with parameters -> accept dynamic data as part of the request [e.g., GET /cats/1 to get cat with id 1])
+    @Get(':id')
+    findOne(@Param() params): string {
+        console.log(params.id);
+        return `This action returns a #${params.id} cat`;
+    }
+    // @Param is used to decorate a method parameter (params in the example above), and makes the route parameters available as properties of that decorated method parameter inside the body of the method. As seen in the code above, we can access the id parameter by referencing params.id. We can also pass in a particular parameter token to the decorator, and then reference the route parameter directly by name in the method body.
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns a #${id} cat`;
+    }
+
 
     /* Nest provides decorators for all of the standard HTTP methods : @Get(), @Post(), @Put(), @Delete(), @Patch(), @Options(), and @Head(). 
     In addition, @All() defines an endpoint that handles all of them. */
